395e17e68754c1cfb77528bd13a626db5eceb19d
Luke Morris <luke@imfuna.com>
2013-07-28 12:31:26 -0700

    Remove FactoryGirl call from everywhere in tests.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,7 +4,7 @@ describe Product, 'all price instance methods' do
   [ :sd_price, :nsd_price, :msp_price, :ip_price, :list_price ].each do |price_method|
     it 'delegates to its product_price' do
       price = double('price', price_method => nil)
-      product = FactoryGirl.build(:product)
+      product = build(:product)
       product.stub(:product_price).and_return(price)
       product.send(price_method)
       expect(price).to have_received(price_method)
