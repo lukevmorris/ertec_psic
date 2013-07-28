6f36a248e3e75c2bb76ff17c152dde4cb1ced383
Luke Morris <luke@imfuna.com>
2013-07-28 12:07:50 -0700

    Delegate price methods to product_price.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   has_one :product_price
+  delegate :sd_price, :nsd_price, :msp_price, :list_price, :ip_price, to: :product_price
 
   GROUPS = [
     'Fencing',
--- app/models/product_price.rb
+++ app/models/product_price.rb
@@ -1,3 +1,14 @@
 class ProductPrice < ActiveRecord::Base
   belongs_to :product
+
+  def sd_price; to_currency(stocking_distributor) end
+  def nsd_price; to_currency(non_stocking_distributor) end
+  def msp_price; to_currency(managed_service_provider) end
+  def ip_price; to_currency(installer_partner) end
+  def list_price; to_currency(list) end
+
+private
+  def to_currency(price_integer)
+    price_integer.to_f / 100
+  end
 end
