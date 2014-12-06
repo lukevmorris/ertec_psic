8fc0addba35585d4d70363b20adadaad98d39c8c
Luke Morris <luke@timelinelabs.com>
2014-12-05 23:49:40 -0800

    Improvements to ZipCodes.

--- zip_codes.rb
+++ zip_codes.rb
@@ -3,15 +3,15 @@ require 'mechanize'
 
 class ZipCodes
   ZIP_STORE = "zip_codes.txt"
+  ZIP_MASTER = "http://www.zipcodestogo.com/California/"
 
   def self.from_file
     File.readlines(ZIP_STORE).map(&:chomp)
   end
 
   def self.refresh_store
-    agent = Mechanize.new
-    agent.get("http://www.zipcodestogo.com/California/")
-    html = Nokogiri::HTML.parse(agent.page.body)
+    page = Mechanize.new.get(ZIP_MASTER)
+    html = Nokogiri::HTML.parse(page.body)
     zip_codes = html.css('td[align="center"] a').children.map(&:text)
     File.open(ZIP_STORE, 'w') { |file| file.puts(zip_codes) }
   end
